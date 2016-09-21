c7630759e10ff02772bf5d05449d57d54fe21a8e
thiagoberaldo1 <thiago._.b.b@hotmail.com>
2016-09-21 19:37:34 -0300

    21/09

--- exercicios/src/main/java/com/github/trete/aula2/Matematica.java
+++ exercicios/src/main/java/com/github/trete/aula2/Matematica.java
@@ -1,12 +1,18 @@
-package com.github.trete.aula2;
+package main.java.com.github.trete.aula2;
 
-import java.util.Scanner;
-
-/**
- * Programa que calcula a soma dos primeiros n números naturais
- */
 public class Matematica {
+    
+ /** 
+ * @param n A quantidade de números naturais a serem somados em ordem
+ * 
+ * @throws IllegalArugumentException Se o número for menor ou igual a 0
+ * 
+ * @return O valor numérico da soma dos primeiros n naturais
+ */
     public static int somaNaturais(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException("valor inválido");
+        }
         int soma;
         
         soma = n * (n + 1) / 2;
@@ -14,7 +20,21 @@ public class Matematica {
         return soma;
     }
     
+   /**
+    * 
+    * @param a Número a ser somado a si mesmo b vezes
+    * 
+    * @param b Número de vezes que a será somado a si mesmo
+    * 
+    * @throws IllegalArgumentExcepion se um dos parâmetros for negativo
+    * 
+    * @return O valor numérico do produto entre a e b
+    */
     public static int produto(int a, int b){
+        if(a < 0 || b < 0){
+            throw new IllegalArgumentException("valor inválido");
+        }
+        
         int produto = 0, índice;
         
         for(índice = 0; índice < b; índice++){
@@ -23,7 +43,23 @@ public class Matematica {
         return produto;
     }
     
+    /**
+     * 
+     * @param base O número a ser elevado ao expoente
+     * 
+     * @param expoente O número ao qual a base será elevada
+     * 
+     * @throws IllegalArgumentException Se a base ou o expoente forem menores
+     * ou igais a 0
+     * 
+     * @return O valor numéirco da potência na qual
+     * base é a base e expoente é o expoente
+     */
     public static int potencia(int base, int expoente){
+        if(base <= 0 || expoente <=0){
+            throw new IllegalArgumentException("valor inválido");
+        }
+        
         int potencia = 1, índice = 1;
         
         while(expoente >= índice){
@@ -33,6 +69,15 @@ public class Matematica {
         return potencia;
     }
     
+    /**
+     * 
+     * @param número O número no qual será verificada a propriedade 3025
+     * 
+     * @throws IllegalArgumentException se o número for menor ou igual a 0 ou 
+     * maior ou igual a 9999
+     * 
+     * @return O valor lógico da existencia da propriedade 3025 em número
+     */
     public static boolean verificaPropriedade3025(int número){
         if(número >= 0 && número<=9999){
             boolean propriedade;
@@ -50,6 +95,15 @@ public class Matematica {
         }
     }
     
+    /**
+     * 
+     * @param n O número no qual será verificada a propriedade 153
+     * 
+     * @throws IllegalArgumentException se o número for menor ou igual a 0 ou 
+     * maior ou igual a 9999
+     * 
+     * @return O valor lógico da existencia da propriedade 153 em número
+     */
     public static boolean verificaPropriedade153(int n){
         if(n < 0 || n>9999){
             throw new IllegalArgumentException("número tem que ser maior que 0"
@@ -65,7 +119,19 @@ public class Matematica {
         return propriedade = (i * i * i + j * j * j + k * k * k) == n;
     }
     
+    /**
+     * 
+     * @param n O número que será verificado se é primo
+     * 
+     * @throws IlegalArgumentException se o número for menor ou igual a 1
+     * 
+     * @return O valor lógico da propriedade de ser primo em n
+     */
     public static boolean verificaPrimo(int n) {
+        if(n <= 1){
+            throw new IllegalArgumentException("o número deve ser maior que 1");
+        }
+        
         int i = 2;
 
         while (i <= (n - 1)) {
@@ -76,7 +142,28 @@ public class Matematica {
         return true;
     }
     
+    /**
+     * 
+     * @param a Maior número no cálculo de maior divisor comum
+     * 
+     * @param b Menor número no cálculo de maior divisor comum
+     * 
+     * @throws IllegalArgumentException se a for maior que b
+     * 
+     * @throws IllegalArgumentException se b for negativo
+     * 
+     * @return O valor numérico do maior divisor comum enre a e b
+     */
     public static int maiorDivisorComum(int a,int b){
+        if(b < a){
+            throw new IllegalArgumentException("o primeiro parâmetro deve ser"
+                    + "maior ou igual ao segundo");
+        }
+        if(b<0){
+            throw new IllegalArgumentException("os parâmetros "
+                    + "devem ser positivos");
+        }
+        
         int m;
         
         while(b != 0){
@@ -90,6 +177,14 @@ public class Matematica {
         return a;
     }
     
+    /**
+     * 
+     * @param n Tamanho da sequencia de números harmônicos somados
+     * 
+     * @throws IllegalArgumentException se n for menor que 1
+     * 
+     * @return O valor numérico do enésimo número harmônico
+     */
     public static double numeroHarmonico(int n){
         if(n < 1){
             throw new IllegalArgumentException("o número deve ser maior "
